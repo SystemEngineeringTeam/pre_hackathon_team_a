1288cdb68d24436802e471dc6377e5cebafd5aa3
momongya <mo5e2ka8@gmail.com>
2021-03-06 17:03:07 +0900

    スタート画面(仮)

---
+++ learn/Assets/script/start.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // //　スタートボタンを押したら実行する
+	// public void StartGame() {
+	// 	SceneManager.LoadScene ("Game");
+	// }
+
+    // //　ゲーム終了ボタンを押したら実行する
+    // public void EndGame() {
+	//     #if UNITY_EDITOR
+	// 	    UnityEditor.EditorApplication.isPlaying = false;
+	//     #elif UNITY_WEBPLAYER
+	// 	    Application.OpenURL("http://www.yahoo.co.jp/");
+	//     #else
+	// 	    Application.Quit();
+	//     #endif
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
