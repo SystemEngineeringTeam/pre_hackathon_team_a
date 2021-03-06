6896ffd641ece30e90e88c4895abd5aeb4a45d17
x19053 <x19053xx@aitech.ac.jp>
2021-03-06 13:38:38 +0900

    メニュー２をつくった

---
+++ learn/Assets/Scenes/menu2/menu2UI.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu2UI : MonoBehaviour
+{
+    public void ChangeScene2Game()
+    {
+        SceneManager.LoadScene("Scenes/SampleScene");
+    }
+}
