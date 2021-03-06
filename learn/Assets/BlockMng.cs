0816a26fdfa19bb969d8c3c592ef086b6f5e8ed7
x20064Naruse <yoy11029mc@gmail.com>
2021-03-06 15:12:17 +0900

    障害物生成

--- learn/Assets/BlockMng.cs
+++ learn/Assets/BlockMng.cs
@@ -7,6 +7,7 @@ public class BlockMng : MonoBehaviour
     public GameObject block;
     float _timer = 0; //_timerの初期化
     float _totalTime = 0;
+    int _cnt = 0;
     void Start()
     {
         
@@ -22,12 +23,20 @@ public class BlockMng : MonoBehaviour
         if(_timer < 0){
             Vector3 position = transform.position;
             //BlockMngから生成
+            position.y = Random.Range(-4, 4);
             GameObject obj = Instantiate(block, position, Quaternion.identity);
             //Instatiate[オブジェクトの生成](コピーする既存obj, 位置, 向き) Quaternion.identity -> 回転しない
             block blockScript = obj.GetComponent<block>();
             
             float speed = 100 + (_totalTime * 10);
             blockScript.SetSpeed(-speed);
+
+            _cnt++;
+            if(_cnt%10 < 3){
+                _timer += 0.1f;
+            }else{
+                _timer += 1;
+            }
 
             _timer += 1;
         }
