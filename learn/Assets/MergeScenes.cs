d9faee5b0e47264c46ff6c37d950911ad385ea52
x20064Naruse <yoy11029mc@gmail.com>
2021-03-06 17:25:36 +0900

    Playerとnaruse01の統合

---
+++ learn/Assets/MergeScenes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MergeScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.LoadLevelAdditive("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
